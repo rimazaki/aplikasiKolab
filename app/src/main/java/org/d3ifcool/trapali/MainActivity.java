f2c794f8944c4e3f13873f71ca39773a31fb509d
rimazakiyatin <rimazakiyatin@gmail.com>
2019-09-11 19:35:46 +0700

    Trapali-FirstCommit

---
+++ app/src/main/java/org/d3ifcool/trapali/MainActivity.java
@@ -0,0 +1,21 @@
+package org.d3ifcool.trapali;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    private CalendarView calendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
+
---
+++ app/src/main/java/org/d3ifcool/trapali/MyIntro.java
@@ -0,0 +1,5 @@
+package org.d3ifcool.trapali;
+
+public class MyIntro {
+
+}
